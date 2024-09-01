a31234dd8331727d29e8b210cd549a20e110826c
Roman Matsypula <r.matsypula@letyshops.com>
2024-09-02 00:21:42 +0300

    create and use custon derective

--- src/app/learning-resources/learning-resources.component.ts
+++ src/app/learning-resources/learning-resources.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { SafeLinkDirective } from '../safe-link.directive';
 
 @Component({
   selector: 'app-learning-resources',
   templateUrl: './learning-resources.component.html',
   styleUrl: './learning-resources.component.css',
   standalone: true,
+  imports: [SafeLinkDirective],
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
-export class LearningResourcesComponent {}
+export class LearningResourcesComponent {
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = confirm('Do you want to leave app?');
+
+    if (wantsToLeave) {
+      return;
+    }
+
+    event.preventDefault();
+  }
+}
